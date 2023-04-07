6bc44d654a5f2fad6910ef17e8428441d7b8bee1
MariaDys <77514289+MariaDys@users.noreply.github.com>
2023-04-07 14:06:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,8 @@ import numpy as np
 chat_id = 734920047 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    n = len(x)
-    x_mean = np.mean(x)
-    a = np.log(x).mean() - 0.5 * np.log(((np.log(x) - np.log(x_mean))**2).mean())
+    def solution(x: np.array) -> float:
+    mu = np.mean(np.log(x) - np.log(647))
+    s2 = np.var(np.log(x) - np.log(647))
+    a = np.exp(mu - s2/2)
     return a
